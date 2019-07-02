8628ed9e56469deb7e2bb72f27ed7254316d320c
Makotek <makoteq@gmail.com>
2019-07-02 10:58:26 +0200

    in work

--- generator.js
+++ generator.js
@@ -6,7 +6,8 @@ module.exports = {
     var mkdirp = require('mkdirp');
     const readline = require('readline');
     const logSymbols = require('log-symbols');
-
+    let theme='white';
+    let themeColor ='black'
     let text = require('./indexmodule.js')
     let name = 'maciek';
     let description = ''
@@ -21,7 +22,8 @@ module.exports = {
       output: process.stdout
     });
     rl.question(chalk.green(logSymbols.warning,'Please enter your name: '), (answer1) => {
-      rl.question(chalk.magenta('Please type something about you : '), (answer2) => {
+      rl.question(chalk.green('Please type something about you : '), (answer2) => {
+       rl.question(chalk.magenta('Select theme, type <dark> or <white>'), (answer3) => {
         rl.question(chalk.green(logSymbols.warning,'Do you want add Facebook? [if yes then write link to your account!]\n-'), (isFb) => {
           if (isFb != '') {
             isfacebook = '';
@@ -40,30 +42,54 @@ module.exports = {
               }
               name = answer1;
               description = answer2;
+         switch(answer3){
+            case 'white':
+              theme='white'
+              themeColor='black';
+              break;
+            case 'dark':
+              theme='black'
+              themeColor='white';
+              break;
+            default:
+              theme='white'
+         }
               copy();
               rl.close();
             });
           });
+          });
         });
       });
     });
-
-
+    /*
+    function downloadImg(){
+      var download = function(uri, filename, callback){
+        request.head(uri, function(err, res, body){
+          request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        });
+      };
+      
+      download('https://raw.githubusercontent.com/makoteq/SHAPE/master/Desktop.png', 'Desktop.png', function(){
+      });
+    }
+*/
     function copy() {
       mkdirp('./' + name + ' portfolio', function (err) {
         if (err) console.error(err)
         else {
-          fs.writeFile('./' + name + ' portfolio/index.html', text.site(name, description, isfacebook, facebooklink, isinstagram, instagramlink, isgithub, githublink),
+          fs.writeFile('./' + name + ' portfolio/index.html', text.site(name, description,theme,themeColor, isfacebook, facebooklink, isinstagram, instagramlink, isgithub, githublink),
             function (err) {
               if (err) throw err;
+              console.log(theme);
               console.log(logSymbols.success,'Saved! Your site has been saved in'+chalk.magenta(' '+name+' portfolio')+' directory  ( ͡° ͜ʖ ͡°)');
             });
-          fs.copyFile('./Desktop.png', name + ' portfolio/Desktop.png', (err) => {
+         /* fs.copyFile('./Desktop.png', name + ' portfolio/Desktop.png', (err) => {
             if (err) throw err;
-          });
+          });*/
         }
       });
 
     }
   }
-}
+  }
--- index.js
+++ index.js
@@ -9,13 +9,5 @@ request = require('request');
 const generator = require('./generator.js');
 let myArgs = process.argv.slice(2);
 if (myArgs[0] == 'create') {
-  var download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-  };
-  
-  download('https://raw.githubusercontent.com/makoteq/SHAPE/master/Desktop.png', 'Desktop.png', function(){
     generator.create();
-  });
 } else console.log(logSymbols.success, chalk.green('Welcome in SHAPE'));
