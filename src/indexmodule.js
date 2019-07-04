5f521934ac9e1c81d3bd305d2fba4dfef85b21f2
Makotek <makoteq@gmail.com>
2019-07-04 15:32:02 +0200

    img

--- src/indexmodule.js
+++ src/indexmodule.js
@@ -13,12 +13,11 @@ integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00
 @import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=swap");\
 </style>\
 <style>\
-body{font-family:"Source Sans Pro",sans-serif;margin:0}a{text-decoration:none;color:#000}.wrapper{background-image:url("desktop.png");background-size:cover;height:100vh;width:100vw;text-align:center}.header{font-family:"Source Sans Pro ",sans-serif;font-size:5em;font-weight:700;margin-bottom:20px}.description{color:#585858;font-weight:700;width:80vw;margin:0 auto}.con{position:absolute;top:50%;transform:translateY(-50%);width:100vw;text-align:center}.con_icons{margin:20px;height:100px}i:nth-of-type(1){font-size:4em;margin:10px;transition:.2s linear}i:nth-of-type(2){font-size:4em;margin:10px;transition:.2s linear}i:nth-of-type(3){font-size:4em;margin:10px;transition:.2s linear}i{cursor:pointer}i:hover{transform:scale(1.5)}\
+body{background-image:url("desktop.png");background-size:cover;font-family:"Source Sans Pro",sans-serif;margin:0}a{text-decoration:none;color:#000}.header{font-family:"Source Sans Pro ",sans-serif;font-size:5em;font-weight:700;margin-bottom:20px}.description{color:#585858;font-weight:700;width:80vw;margin:0 auto}.con{position:absolute;top:50%;transform:translateY(-50%);width:100vw;text-align:center}.con_icons{margin:20px;height:100px}i:nth-of-type(1){font-size:4em;margin:10px;transition:.2s linear}i:nth-of-type(2){font-size:4em;margin:10px;transition:.2s linear}i:nth-of-type(3){font-size:4em;margin:10px;transition:.2s linear}i{cursor:pointer}i:hover{transform:scale(1.5)}\
 </style>\
 <title>This webside belongs to '+name+'</title>\
 </head>\
 <body>\
-<div class="wrapper">\
 <div class="con">\
 <p class="header">'+name+'</p>\
 <p class="description">\
@@ -28,7 +27,6 @@ body{font-family:"Source Sans Pro",sans-serif;margin:0}a{text-decoration:none;co
 <a href="'+facebooklink+'" target="_blank"><i style="display:'+isfacebook+'" class="fab fa-facebook-square"></i></a>\
 <a href="'+instagramlink+'" target="_blank"> <i style="display:'+isinstagram+'" class="fab fa-instagram"></i></a>\
 <a href="'+githublink+'" target="_blank"><i style="display:'+isgithub+'" class="fab fa-github"></i>\
-</div>\
 </div>\
 </div>\
 </body>\
